35b1c0840452d45244a3765638e72521ed2bf709
Gabriel Adomnicai <gabesoft@gmail.com>
2015-05-31 22:19:44 -0700

    Rules and unit tests - in progress

---
+++ test/rules.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var expect = require('chai').expect
+  , fs     = require('fs')
+  , path   = require('path')
+  , State  = require('../lib/state').State
+  , Token  = require('../lib/token').Token
+  , rules  = require('../lib/rules');
+
+var fixtures = {
+        'headline' : {
+            content  : 'INTRODUCTION                                    *abolish* *:Abolish* *:Subvert*'
+          , nonMatch : 'Not a headline'
+          , results : [ [ 'INTRODUCTION                                    ' ], [ 'text',  '*abolish* *:Abolish* *:Subvert*' ] ]
+        }
+      , 'header' : {
+            content  : 'Values: A string of characters separated by spaces.                          ~'
+          , nonMatch : 'Not a header'
+          , results  : [ [ 'Values: A string of characters separated by spaces.                          ' ] ]
+        }
+      , 'vim-option' : {
+            content  : "and 'tabstop' options and the 'filetype' to 'help'.  Never set a global option"
+          , nonMatch : 'Not an option'
+          , results  : [ ['text', 'and ' ], [ "'tabstop'" ], [ 'text', ' options and the ' ], [ "'filetype'" ], [ 'text', ' to ' ], [ "'help'" ], [ 'text', '.  Never set a global option' ] ]
+        }
+      , 'tag-name' : {
+            content  : '*:helpfind* *:helpf*'
+          , nonMatch : 'Not a tag name'
+          , results  : [ [ ':helpfind' ], [ 'text', ' ' ], [ ':helpf' ] ]
+        }
+      , 'tag-link' : {
+            content  : '			|quickfix| commands, e.g., |:cnext| to jump to the'
+          , nonMatch : 'Not a tag link'
+          , results  : [ [ 'text', '			' ], [ 'quickfix'], [ 'text', ' commands, e.g., ' ], [ ':cnext' ], [ 'text', ' to jump to the' ] ]
+        }
+    };
+
+
+describe('rules', function () {
+    var state = null;
+
+    beforeEach(function () {
+        state = new State();
+    });
+
+    Object.keys(fixtures).forEach(function (rule) {
+        describe(rule, function () {
+            describe('when the rule matches', function () {
+                beforeEach(function () {
+                    state.pushToken('text', fixtures[rule].content);
+                    rules.applyRule(rule, state);
+                });
+
+                it('adds the correct number of tokens', function () {
+                    expect(state.tokens.length).to.equal(fixtures[rule].results.length);
+                });
+
+                fixtures[rule].results.forEach(function (result, index) {
+                    var type    = result.length === 1 ? rule : result[0]
+                      , content = result.length === 1 ? result[0] : result[1];
+
+                    it('adds the correct token type at position ' + index, function () {
+                        expect(state.tokens[index].type).to.equal(type);
+                    });
+
+                    it('adds the correct token content at position ' + index, function () {
+                        expect(state.tokens[index].content).to.equal(content);
+                    });
+                });
+            });
+
+            describe('when the rule does not match', function () {
+                it('does not change the state', function () {
+                    state.pushToken('text', fixtures[rule].nonMatch);
+                    rules.applyRule(rule, state);
+                    expect(state.tokens.length).to.equal(1);
+                    expect(state.tokens[0].type).to.equal('text');
+                    expect(state.tokens[0].content).to.equal(fixtures[rule].nonMatch);
+                });
+            });
+        });
+    });
+});
--- test/token.js
+++ test/token.js
@@ -10,24 +10,45 @@ describe('Token', function () {
 
     beforeEach(function () {
         d = fs.readFileSync(path.join(__dirname, './data/doc8.txt'), 'utf8');
-        t = new Token(d);
+        t = new Token('text', d);
         t.markLines();
     });
 
-    it('marks lines info', function () {
-        expect(t.lnCount).to.equal(13);
-        expect(t.lnBegin).to.eql([0, 13, 14, 32, 63, 93, 125, 158, 202, 238, 261, 287, 322, 323])
-        expect(t.lnEnd).to.eql([12, 13, 31, 62, 92, 124, 157, 201, 237, 260, 286, 321, 322, 323])
-        expect(t.lnIndent).to.eql([0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 323]);
+    describe('markLines', function () {
+        it('returns the correct number of lines', function () {
+            expect(t.lnCount).to.equal(14);
+        });
+
+        it('marks the begin position of each line', function () {
+            expect(t.lnBegin).to.eql([0, 13, 14, 32, 63, 93, 125, 158, 202, 238, 261, 287, 322, 323, 333])
+        });
+
+        it('marks the end position of each line', function () {
+            expect(t.lnEnd).to.eql([12, 13, 31, 62, 92, 124, 157, 201, 237, 260, 286, 321, 322, 332, 333])
+        });
+
+        it('marks the indent of each line', function () {
+            expect(t.lnIndent).to.eql([0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 333]);
+        });
     });
 
-    it('gets specific lines', function () {
-        var lines = t.getLines(4, 7);
+    describe('getLines', function () {
+        it('gets specific lines', function () {
+            var lines = t.getLines(4, 7);
 
-        expect(lines).to.equal([
-            '  - Completion string ranking'
-          , '  - General semantic completion'
-          , '  - C-family semantic completion'
-        ].join('\n'));
+            expect(lines).to.equal([
+                '  - Completion string ranking'
+              , '  - General semantic completion'
+              , '  - C-family semantic completion'
+            ].join('\n'));
+        });
+
+        it('gets one line', function () {
+            expect(t.getLines(0, 1)).to.equal('- User Guide');
+        });
+
+        it('gets the last line', function () {
+            expect(t.getLines(t.lnCount - 1, t.lnCount)).to.equal('Last line');
+        });
     });
 });
